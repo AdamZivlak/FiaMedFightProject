b2eac60524e77f63c8ca3045d4b613490ddfd4c2
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-02-27 18:54:18 +0100

    MBG-66 on click: hides button, then displays animation, then shows button with the result dice face up

--- FiaMedFight/MainPage.xaml.cs
+++ FiaMedFight/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -23,19 +25,35 @@ namespace FiaMedFight
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        static Storyboard spinAnimation;
+
         public MainPage()
         {
             this.InitializeComponent();
-        }
 
-
+            // Get the storyboard animation from the resource dictionary
+            spinAnimation = this.Resources["SpinAnimation"] as Storyboard;
+            Storyboard.SetTarget(spinAnimation, image);
+        }        
+        
         private void SimpleDice_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
+        {            
             var button = sender as Button;
             int result = Dice.RollAnyDice(6);
-            ResultText.Text = "You rolled: " + result;
+            button.Visibility = Visibility.Collapsed;
+            image.Visibility = Visibility.Visible;
+
+            spinAnimation.Begin();
             Dice.ChangeDiceFace(button, result);
-        }
+
+            //waits for animation to finish
+            spinAnimation.Completed += delegate (object self, object btn)
+            {
+                image.Visibility = Visibility.Collapsed;
+                button.Visibility = Visibility.Visible;
+                ResultText.Text = "You rolled: " + result;
+            };
+        }     
 
         private void pointerout(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
