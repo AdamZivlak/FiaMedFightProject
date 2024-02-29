8d096e2f2bda920777b08063bb325644719ba2b1
Minna de Verdier <minna.deverdier@gmail.com>
2024-02-29 15:30:39 +0100

    Template for rules overlay

---
+++ FiaMedFight/Templates/StringBindingCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiaMedFight.Templates
+{
+    internal class StringBindingCollection
+    {
+        public string Rules { get; set; }
+        public string Title { get; set; }
+
+        public static StringBindingCollection GetCollection()
+        {
+            var collection = new StringBindingCollection()
+            {
+                Rules = "Här är regler\n" +
+                "* Regel 1\n" +
+                "* Regel 2\n" +
+                "* Regel 3",
+                Title = "Regler"
+            };
+            return collection;
+        }
+
+       
+        
+    }
+}
