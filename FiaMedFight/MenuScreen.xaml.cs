bac440887b67f1eec803e8776345e990a19c15dd
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-03-05 12:01:56 +0100

    MBG-27 Created rules popup and template for different types of textBlock-styles

--- FiaMedFight/MenuScreen.xaml.cs
+++ FiaMedFight/MenuScreen.xaml.cs
@@ -14,69 +14,130 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace FiaMedFight
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Represents the menu screen of the application.
     /// </summary>
     public sealed partial class MenuScreen : Page
-    {      
+    {
+        /// <summary>
+        /// Collection of string bindings for game rules.
+        /// </summary>
+        static StringBindingCollection ruleStrings = new StringBindingCollection(0);
 
-        static StringBindingCollection ruleStrings = new StringBindingCollection();
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuScreen"/> class.
+        /// </summary>
         public MenuScreen()
         {
             this.InitializeComponent();
             ApplicationExitButton.Click += ApplicationExitButton_Click;
         }
 
-        //stänger programmet
+        /// <summary>
+        /// Click to exit the application.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void ApplicationExitButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Exit();
         }
 
+        /// <summary>
+        /// Click to open the game options screen, to select number of players for the new game.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void NewGameButton_Click(object sender, RoutedEventArgs e)
         {
             //SelectPlayersPopup.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Click to navigate to the main game page.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void GameStartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Skapa en instans av MainPage
+            // Create an instance of MainPage
             MainPage mainPage = new MainPage();
 
-            // Navigera till MainPage
+            // Navigate to MainPage
             Frame.Navigate(typeof(MainPage));
-
         }
 
+        /// <summary>
+        /// Click to show the game rules popup.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void RulesOpenButton_Click(object sender, RoutedEventArgs e)
         {
             Dimmer.Visibility = Visibility.Visible;
             RulesPopup.Visibility = Visibility.Visible;
-            NextButton.Tag = 0;
+
+            PrevButton.Visibility = Visibility.Collapsed;
+            NextButton.Visibility = Visibility.Visible;
+
+            RulesOpenButton.Tag = 0;
             RulesHeaderTextBlock.Text = ruleStrings.Title[0];
             RulesBodyTextBlock.Text = ruleStrings.Rules[0];
         }
 
+        /// <summary>
+        /// Click to close the game rules popup.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void ReturnButton_Click(object sender, RoutedEventArgs e)
         {
             RulesPopup.Visibility = Visibility.Collapsed;
             Dimmer.Visibility = Visibility.Collapsed;
             GameStartButton.Visibility = Visibility.Visible;
-            
         }
 
+        /// <summary>
+        /// Click to navigate to the next page of game rules.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void RulesNextButton_Click(object sender, RoutedEventArgs e)
         {
-            var rulePage = sender as Button;
-            int i = (int)rulePage.Tag + 1;
-            rulePage.Tag = i;
-            RulesHeaderTextBlock.Text = ruleStrings.Title[i];
-            RulesBodyTextBlock.Text = ruleStrings.Rules[i];
+            if (PrevButton.Visibility != Visibility.Visible)
+                PrevButton.Visibility = Visibility.Visible;
+
+            int pageIndex = (int)RulesOpenButton.Tag + 1;
+            RulesOpenButton.Tag = pageIndex;
+
+            RulesHeaderTextBlock.Text = ruleStrings.Title[pageIndex];
+            RulesBodyTextBlock.Text = ruleStrings.Rules[pageIndex];
+
+            if (pageIndex + 1 == ruleStrings.Rules.Count)
+                NextButton.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Click to navigate to the previous page of game rules.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void RulesPreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NextButton.Visibility != Visibility.Visible)
+                NextButton.Visibility = Visibility.Visible;
+
+            int pageIndex = (int)RulesOpenButton.Tag - 1;
+            RulesOpenButton.Tag = pageIndex;
+
+            RulesHeaderTextBlock.Text = ruleStrings.Title[pageIndex];
+            RulesBodyTextBlock.Text = ruleStrings.Rules[pageIndex];
+
+            if (pageIndex == 0)
+                PrevButton.Visibility = Visibility.Collapsed;
         }
     }
 }
--- FiaMedFight/Templates/StringBindingCollection.cs
+++ FiaMedFight/Templates/StringBindingCollection.cs
@@ -1,30 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
 
 namespace FiaMedFight.Templates
 {
+    /// <summary>
+    /// Represents a collection of string bindings for the game.
+    /// </summary>
     public class StringBindingCollection
     {
+        /// <summary>
+        /// Gets the titles associated with the game rule pages.
+        /// </summary>
+        public List<string> Title { get; private set; } = new List<string>();
 
-        
-        public string[] Rules { get;  private set; } = new string[6];
-        public string[] Title { get; private set; } = new string[6];
+        /// <summary>
+        /// Gets the rules associated with the game pages.
+        /// </summary>
+        public List<string> Rules { get; private set; } = new List<string>();
 
-        public StringBindingCollection()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringBindingCollection"/> class. 
+        /// <list type="number">Collection of rules
+        /// <item>Not Yet Implemented</item>
+        /// </list>
+        /// </summary>
+        /// <param name="collection">Determines which string collection to use</param>
+        public StringBindingCollection(int collection)
         {
-            Title[0] = "Mål:";
-            Title[1] = "Regler";
-            Rules[0] ="Försök att flytta dina pjäser runt spelplanen och nå målet innan din motståndare. Spelarna slåss genom att slå tärningen och skicka varandra 'hem' till boet.";
-            Rules[1] = "Här är regler\n" +
-            "* Regel 1\n" +
-            "* Regel 2\n" +
-            "* Regel 3";
+            switch(collection)
+            {
+                case 0: SetRuleStrings(); break;
+                default: break;
+            }
         }
 
+        private void SetRuleStrings() { 
+            Title.Insert(0, "Mål");
+            Rules.Insert(0, "Försök att flytta dina pjäser runt spelplanen och nå målet innan din motståndare. \n" + 
+                "Spelarna slåss genom att slå tärningen och skicka varandra tillbaka till start.\n" +
+                "Den första spelaren som lyckas flytta alla sina pjäser runt spelplanen och nå målet vinner spelet.");
+            
+            Title.Insert(1, "Spelupplägg");
+            Rules.Insert(1, "Start: Varje spelare tilldelas ett lag och 4 pjäser placeras i deras bo på spelplanen.\n" +
+            "Turordning: Spelarna turas om att slå tärningen och väljer sedan vilken pjäs de vill flytta så många steg som tärningen visar.\n" +
+            "Flytta pjäsen: Efter att ha slagit tärningen, flyttar spelaren en av sina pjäser framåt på spelplanen med det antal steg som tärningen visar. Endast en pjäs kan flyttas per tärningsslag. \n" +
+            "Utgång från boet: För att komma ut från boet måste en spelare slå antingen en etta eller en sexa på tärningen.\n" +
+            "Kollision: Om en spelares pjäs hamnar på samma ruta som motståndarens pjäs, utlöses en strid. Båda spelarna slår tärningen, den spelare som får högst värde vinner striden och får stå kvar. Motståndarens pjäs flyttas tillbaka 'hem' till deras bo. Om båda slår lika vinner den pjäs som startade striden, dvs den aktiva spelarens pjäs." );
 
-
+            Title.Insert(2, "Genvägar");
+            Rules.Insert(2, "Om en spelare landar på en ruta med en genväg kan de flytta ett antal extra steg framåt, men har man otur kan en genväg även vara en “senväg”: \n" +
+            "    - Om spelaren slår en etta eller sexa får den gå två steg framåt. \n" +
+            "    - Om spelaren slår en tvåa eller fyra får den gå ett steg bakåt. \n" +
+            "    - Om spelaren slår en trea eller femma står den kvar på samma ruta.");
+            
+            Title.Insert(3, "Fällor");
+            Rules.Insert(3, "Om en spelare hamnar på en ruta med en fälla kan olika händelser inträffa:\n" +
+            "    - Spelaren står över ett kast: Spelaren missar sin nästa tur och får inte slå tärningen.\n" +
+            "    - Flytta bak ett antal steg: Pjäsen flyttas tillbaka ett antal steg på spelplanen.\n" +
+            "    - Slå en etta eller sexa: Pjäsen kan bara flytta med en etta, en sexa, eller vid en kollision av en annan pjäs. Spelarens övriga pjäser kan flytta som vanligt");
+        }
     }
 }
